58f263506933b854d5a7e537db85d818bc98454c
michael1241 <michael@jamcupboard.co.uk>
2020-02-01 10:28:20 +0000

    flourish working

--- graph.py
+++ graph.py
@@ -23,12 +23,14 @@ else:
 
 resolution = 60000 #every 60 seconds
 starttime = 1579114800000 #maybe generate automatically from first game end time
-endtime = 1579122000000
+endtime = 1579122000000 + 1
+
+timepoints = range(starttime, endtime, resolution)
 
 players = {p: [[starttime, 0]] for p in graphdata.keys()}
 
 
-for timepoint in range(starttime, endtime, resolution):
+for timepoint in timepoints[1:]:
     for player, scores in graphdata.items():
         try:
             if scores[0][0] < timepoint:
@@ -36,41 +38,21 @@ for timepoint in range(starttime, endtime, resolution):
             else:
                 players[player].append([timepoint, players[player][-1][-1]]) #append current timepoint with with last recorded score
         except IndexError:
-            pass
+            players[player].append([timepoint, players[player][-1][-1]])
 
 frames = []
 
-for player, results in players.items():
-    df = pd.DataFrame(results, columns =['time', 'score'], dtype = int)
+for player, scores in players.items():
+    df = pd.DataFrame(scores, columns =['time', 'score'], dtype = int)
     df['player'] = player
     frames.append(df)
 data = pd.concat(frames)
+data = data.pivot(index='player', columns='time', values='score')
 
-r = lambda: random.randint(0,255)
-colors = dict(zip(players.keys(), ['#%02X%02X%02X' % (r(),r(),r()) for x in range(len(players))]))
+new_format = []
+for col in data.columns:
+    new_format.append(datetime.utcfromtimestamp(col//1000).strftime('%H:%M'))
+data.columns = new_format
 
-def drawChart(current_time):
-    dff = (data[data['time'].eq(current_time)]
-           .sort_values(by='score', ascending=False)
-           .head(10))
-    ax.clear()
+data.to_csv('graphoutput.csv')
 
-    dff = dff[::-1]
-    ax.barh(dff['player'], dff['score'], color=[colors[x] for x in dff['player']])
-
-    clock = datetime.utcfromtimestamp(current_time//1000).strftime('%H:%M') #%H:%M:%S if adding seconds
-
-    for i, (score, player) in enumerate(zip(dff['score'], dff['player'])):
-        ax.text(score, i, player, ha='right')
-        ax.text(score, i, score, ha='left')
-    ax.text(1, 0.4, clock, transform=ax.transAxes, color='#777777', size=46, ha='right', weight=800)
-    ax.tick_params(axis='x', colors='#777777', labelsize=12)
-    ax.set_yticks([])
-    ax.grid(which='major', axis='x', linestyle='-')
-    ax.set_axisbelow(True)
-    ax.text(0, 1.05, 'Lichess Titled Arena Jan 2020 Replay', transform=ax.transAxes, size=24, weight=600, ha='left')
-
-fig, ax = plt.subplots(figsize=(15, 8))
-animator = animation.FuncAnimation(fig, drawChart, frames=range(starttime+resolution, endtime, resolution))
-
-animator.save('file.mp4')
